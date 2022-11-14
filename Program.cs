e19c6e0f1088a2f2455fb79e8de26efef5b312da
yaroslav <95yzbek@gmail.com>
2022-11-14 22:17:41 +0500

    Первый вариант

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[]arrayOne=new string[5]{"array","5","ten","11","rock-n-roll"};
+string[]arrayTwo=new string[arrayOne.Length];
+ChangeArray(arrayOne,arrayTwo);
+Print(arrayTwo);
+void ChangeArray(string[]arrayOne,string[]arrayTwo)
+{
+    int count=0;
+    for(int i=0;i<arrayOne.Length;i++)
+    {
+        if(arrayOne[i].Length<=3)
+        {
+            arrayTwo[count]=arrayOne[i];
+            count++;
+        }
+    }
+}
+void Print(string[]array)
+{
+    for(int i=0;i<array.Length;i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
